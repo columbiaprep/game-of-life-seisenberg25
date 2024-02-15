20646d31850e564117d99ef1a9ce20d631fe6593
sophieeisenberg99 <sophieeisenberg99@gmail.com>
2024-02-15 11:39:40 -0500

    finished

--- Board.java
+++ Board.java
@@ -17,6 +17,17 @@ public class Board {
       }
     }
   }
+  public boolean checkEnd(){
+    boolean end = false;
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if(board[i][j].getIsAlive()){
+          end = true;
+        }
+      }
+    }
+    return end;
+  }
 
   //prints the board
   public void displayBoard() {
@@ -32,23 +43,108 @@ public class Board {
   //places first generation of bacteria on board
   public void placeFirstGen() {
     board[0][3] = new Cell(true);
+    board[0][4] = new Cell(true);
     board[4][4] = new Cell(true);
+    board[3][3] = new Cell(true);
+    board[4][3] = new Cell(true);
+    board[4][5] = new Cell(true);
+    board[3][4] = new Cell(true);
   }
 
   //counts the number of neighbors who are alive, returns the result as an integer
 	//counts all eighth neighboring spaces
   public int countLiveNeighbors(int i, int j) {
-    
-    return 0;
+    int count = 0;
+    if(i>0&&j>0) {
+      if (board[i - 1][j - 1].getIsAlive()) {
+        count += 1;
+      }
+    }
+    if(i>0){
+      if(board[i-1][j].getIsAlive()){
+        count+=1;
+      }
+    }
+    if(i>0&&j<board.length-1){
+      if(board[i-1][j+1].getIsAlive()){
+        count+=1;
+      }
+    }
+    if(j>0){
+      if(board[i][j-1].getIsAlive()){
+        count+=1;
+      }
+    }
+    if(j>0&&i<board.length-1){
+      if(board[i+1][j-1].getIsAlive()){
+        count+=1;
+      }
+    }
+    if(i<board.length-1&&j<board.length-1){
+      if(board[i+1][j+1].getIsAlive()){
+        count+=1;
+      }
+    }
+    if(j<board.length-1) {
+      if (board[i][j + 1].getIsAlive()) {
+        count += 1;
+      }
+    }
+    if(i<board.length-1) {
+      if (board[i + 1][j].getIsAlive()) {
+        count += 1;
+      }
+    }
+
+//    for(int x = j-1; x<j+1; j++) {
+//      if(j == 0){
+//        x = 1;
+//      }
+//      for (int c = i - 1; c < i + 1; i++) {
+//        if(i==0){
+//          c = 1;
+//        }
+//        if(x!=i||c!=j) {
+//          if (board[x][c].getIsAlive()) {
+//            count += 1;
+//          }
+//        }
+//      }
+//    }
+    return count;
   }
 
   public void createNewGeneration() {
     //creates a blank board of same size called newGenBoard
-    Cell[][] nextGenBoard = new Cell[board.length][board[0].length]; 
+    Cell[][] nextGenBoard = new Cell[board.length][board[0].length];
+    for (int i = 0; i < nextGenBoard.length; i++) {
+      for (int j = 0; j < nextGenBoard[i].length; j++) {
+        nextGenBoard[i][j] = new Cell(false);
+      }
+    }
+//    System.out.print("hi");
+
     //all changes should be reflected only on nextGenBoard, and we copy them over on the last line of the method
 
 	//for each space in the nextGenBoard:
-		
+	for(int r = 0; r<nextGenBoard.length; r++){
+      for(int c = 0; c<nextGenBoard[r].length; c++){
+        int number = countLiveNeighbors(r,c);
+        if(board[r][c].getIsAlive()){
+          if(number==2||number==3) {
+            nextGenBoard[r][c].setIsAlive(true);
+          }
+//          System.out.print("hi");
+        }
+        else{
+          if(number==3) {
+            nextGenBoard[r][c].setIsAlive(true);
+//            System.out.print("hi");
+          }
+        }
+      }
+    }
+//    System.out.print("hi");
       //a live cell with 2-3 neighbors survives
       
 
--- Simulation.java
+++ Simulation.java
@@ -10,7 +10,7 @@ public class Simulation {
   }
 
   public void startSimulation() { 
-    while (true) {
+    while (board.checkEnd()) {
       pause(); 
       tick();
     }
